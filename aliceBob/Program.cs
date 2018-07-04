706e347da72cd43ddbe29483323f7191a4ff883f
oge <oge.nwike@gmail.com>
2018-07-04 15:12:49 +0100

    Using the System.Text Api to read the array content from a text file

--- aliceBob/Program.cs
+++ aliceBob/Program.cs
@@ -1,34 +1,22 @@
-﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace aliceBob
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-/* 
-            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true); */
+class Solution {
 
-            //int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp));
-
-            //int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), bTemp => Convert.ToInt32(bTemp));
-
-            int[] a = {4, 2, 8};
-            int[] b = {2, 1, 10};
-
-            int[] result = solve(a, b);
-
-            Console.WriteLine(string.Join(" ", result));
-
-            /* textWriter.Flush();
-            textWriter.Close(); */
-        }
-
-        static int[] solve(int[] a, int[] b)
-        {
-            int alicePoint = 0;
+    // Complete the solve function below.
+    static int[] solve(int[] a, int[] b) {
+        int alicePoint = 0;
             int bobPoint = 0;
 
             for(int i = 0; i < 3; i++)
@@ -49,6 +37,22 @@ namespace aliceBob
 
             int[] result = {alicePoint, bobPoint};
             return result;
-        }
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+
+        int[] b = Array.ConvertAll(Console.ReadLine().Split(' '), bTemp => Convert.ToInt32(bTemp))
+        ;
+        int[] result = solve(a, b);
+
+        textWriter.WriteLine(string.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
